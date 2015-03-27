01f92e2748d004401a61c1faa5f7013e13facf34
vvasiloud <vvasiloud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 19:49:41 +0000

    Minor Update
    git-svn-id: https://plugins.svn.wordpress.org/loltracker/tags/1.0.1@1122424 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-lol-tracker-deactivator.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/vvasiloud/wp-loltracker
+ * @since      1.0.0
+ *
+ * @package    LoL_Tracker
+ * @subpackage LoL_Tracker/includes
+ */
+
+/**
+ * @since      1.0.0
+ * @package    LoL_Tracker
+ * @subpackage LoL_Tracker/includes
+ * @author     Vasilis Vasiloudis <vvasiloudis@gmail.com>
+ */
+class LoL_Tracker_Deactivator {
+
+	/**
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
